f58a600d090354d4dae31f3e48b79d8d3ae5ae8c
Robert Niyitanga <niroberto>
2023-11-21 09:34:46 +0200

    feat: implemation of user-friendly contact page

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "../componets/Form";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};
+
+export default ContactPage;
